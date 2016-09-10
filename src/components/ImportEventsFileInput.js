9f7fff7f6aa66b03dad006ee4dc6278c2130b64e
Corey McDermott <corey@mcdermott.id.au>
2016-09-10 19:35:43 +1000

    Code cleanup.

--- src/components/ImportEventsFileInput.js
+++ src/components/ImportEventsFileInput.js
@@ -1,4 +1,8 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react';
+
+const propTypes = {
+  handleImport: PropTypes.func.isRequired,
+};
 
 class ImportEventsFileInput extends Component {
   constructor(props) {
@@ -7,22 +11,24 @@ class ImportEventsFileInput extends Component {
   }
 
   onChange(e) {
-    const { handleImport } = this.props
+    const { handleImport } = this.props;
     const file = e.target.files[0];
     const reader = new FileReader();
     reader.onload = function(data) {
-      handleImport(data.target.result)
-    }
+      handleImport(data.target.result);
+    };
     reader.readAsBinaryString(file);
   }
 
   render() {
     return (
       <label className="btn btn-default btn-file m-right">
-          Import Events <input type="file" onChange={this.onChange} className="hidden-input" />
+        Import Events <input type="file" onChange={this.onChange} className="hidden-input" />
       </label>
-    )
+    );
   }
 }
 
-export default ImportEventsFileInput
+ImportEventsFileInput.propTypes = propTypes;
+
+export default ImportEventsFileInput;
