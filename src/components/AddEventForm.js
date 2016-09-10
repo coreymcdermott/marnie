6ad400b36f86ed295170ecf8d80765a43a4164ca
Corey McDermott <corey@mcdermott.id.au>
2016-09-10 19:16:08 +1000

    Code cleanup.

--- src/components/AddEventForm.js
+++ src/components/AddEventForm.js
@@ -1,4 +1,8 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react';
+
+const propTypes = {
+  handleAdd: PropTypes.function.isRequire,
+};
 
 class AddEventForm extends Component {
   constructor(props) {
@@ -10,68 +14,70 @@ class AddEventForm extends Component {
     e.preventDefault();
     const { handleAdd } = this.props;
     handleAdd(
-      document.getElementById("inputDate").value,
-      document.getElementById("inputCountry").value,
-      document.getElementById("inputIndicator").value,
-      document.getElementById("inputPeriod").value,
-      document.getElementById("inputForecast").value,
-      document.getElementById("inputActual").value,
-      document.getElementById("inputTime").value
+      document.getElementById('inputDate').value,
+      document.getElementById('inputCountry').value,
+      document.getElementById('inputIndicator').value,
+      document.getElementById('inputPeriod').value,
+      document.getElementById('inputForecast').value,
+      document.getElementById('inputActual').value,
+      document.getElementById('inputTime').value
     );
-    $("#addEventModal").modal('hide');
+    $('#addEventModal').modal('hide');
   }
 
   render() {
     return (
       <form className="form-horizontal" id="AddEventForm" onSubmit={this.onSubmit}>
         <div className="form-group">
-            <label className="col-sm-2 control-label">Date</label>
-            <div className="col-sm-10">
-              <input type="text" className="form-control" id="inputDate" placeholder=""/>
-            </div>
+          <label className="col-sm-2 control-label">Date</label>
+          <div className="col-sm-10">
+            <input type="text" className="form-control" id="inputDate" placeholder="" />
+          </div>
         </div>
         <div className="form-group">
-            <label className="col-sm-2 control-label">Country</label>
-            <div className="col-sm-10">
-                <input type="text" className="form-control" id="inputCountry" placeholder="" />
-            </div>
+          <label className="col-sm-2 control-label">Country</label>
+          <div className="col-sm-10">
+            <input type="text" className="form-control" id="inputCountry" placeholder="" />
+          </div>
         </div>
         <div className="form-group">
-            <label className="col-sm-2 control-label">Indicator</label>
-            <div className="col-sm-10">
-                <input type="text" className="form-control" id="inputIndicator" placeholder="" />
-            </div>
+          <label className="col-sm-2 control-label">Indicator</label>
+          <div className="col-sm-10">
+            <input type="text" className="form-control" id="inputIndicator" placeholder="" />
+          </div>
         </div>
         <div className="form-group">
-            <label className="col-sm-2 control-label">Period</label>
-            <div className="col-sm-10">
-                <input type="text" className="form-control" id="inputPeriod" placeholder="" />
-            </div>
+          <label className="col-sm-2 control-label">Period</label>
+          <div className="col-sm-10">
+            <input type="text" className="form-control" id="inputPeriod" placeholder="" />
+          </div>
         </div>
         <div className="form-group">
-            <label className="col-sm-2 control-label">Forcast</label>
-            <div className="col-sm-10">
-                <input type="text" className="form-control" id="inputForecast" placeholder="" />
-            </div>
+          <label className="col-sm-2 control-label">Forcast</label>
+          <div className="col-sm-10">
+            <input type="text" className="form-control" id="inputForecast" placeholder="" />
+          </div>
         </div>
         <div className="form-group">
-            <label className="col-sm-2 control-label">Actual</label>
-            <div className="col-sm-10">
-                <input type="text" className="form-control" id="inputActual" placeholder="" />
-            </div>
+          <label className="col-sm-2 control-label">Actual</label>
+          <div className="col-sm-10">
+            <input type="text" className="form-control" id="inputActual" placeholder="" />
+          </div>
         </div>
         <div className="form-group">
-            <label className="col-sm-2 control-label">Time</label>
-            <div className="col-sm-10">
-                <input type="text" className="form-control" id="inputTime" placeholder=""/>
-            </div>
+          <label className="col-sm-2 control-label">Time</label>
+          <div className="col-sm-10">
+            <input type="text" className="form-control" id="inputTime" placeholder="" />
+          </div>
         </div>
-        <div className='clear-fix hack'>
+        <div className="clear-fix hack">
           <button type="submit" className="btn btn-default pull-right">Add Event</button>
         </div>
       </form>
-    )
+    );
   }
 }
 
-export default AddEventForm
+AddEventForm.propTypes = propTypes;
+
+export default AddEventForm;
