66f84a829bb44f2d1f408d9fbdae04b46474aa0f
Corey McDermott <corey@mcdermott.id.au>
2016-09-10 19:52:58 +1000

    Code cleanup.

--- src/reducers.js
+++ src/reducers.js
@@ -1,44 +1,44 @@
-import { combineReducers } from 'redux'
-import uuid from 'uuid'
-import { ADD_EVENT, DELETE_EVENT, IMPORT_EVENTS, SET_MARKET_FILTER } from './actions'
+import { combineReducers } from 'redux';
+import uuid from 'uuid';
+import { ADD_EVENT, DELETE_EVENT, IMPORT_EVENTS, SET_MARKET_FILTER } from './actions';
 
 function events(state = [], action) {
   switch (action.type) {
-    case ADD_EVENT:
-      return [
-        ...state,
-        {
-          uuid:      uuid.v4(),
-          date:      action.date,
-          country:   action.country,
-          indicator: action.indicator,
-          period:    action.period,
-          forecast:  action.forecast,
-          actual:    action.actual,
-          time:      action.time
-        }
-      ]
-    case DELETE_EVENT:
-      return state.filter(event => event.uuid !== action.uuid)
-    case IMPORT_EVENTS:
-      return action.events
-    default:
-      return state
+  case ADD_EVENT:
+    return [
+      ...state,
+      {
+        uuid:      uuid.v4(),
+        date:      action.date,
+        country:   action.country,
+        indicator: action.indicator,
+        period:    action.period,
+        forecast:  action.forecast,
+        actual:    action.actual,
+        time:      action.time,
+      },
+    ];
+  case DELETE_EVENT:
+    return state.filter(event => event.uuid !== action.uuid);
+  case IMPORT_EVENTS:
+    return action.events;
+  default:
+    return state;
   }
 }
 
 function market(state = 'ALL', action) {
   switch (action.type) {
-    case SET_MARKET_FILTER:
-      return action.filter
-    default:
-      return state
+  case SET_MARKET_FILTER:
+    return action.filter;
+  default:
+    return state;
   }
 }
 
 const rootReducer = combineReducers({
-    events,
-    market,
+  events,
+  market,
 });
 
-export default rootReducer
+export default rootReducer;
