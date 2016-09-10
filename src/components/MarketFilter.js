be1cb0b42fb0861be44c27011254749ad3399f47
Corey McDermott <corey@mcdermott.id.au>
2016-09-10 19:40:12 +1000

    Code cleanup.

--- src/components/MarketFilter.js
+++ src/components/MarketFilter.js
@@ -1,4 +1,8 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react';
+
+const propTypes = {
+  setMarketFilter: PropTypes.func.isRequired,
+};
 
 class MarketFilter extends Component {
   render() {
@@ -10,14 +14,16 @@ class MarketFilter extends Component {
           <span className="caret"></span>
         </button>
         <ul className="dropdown-menu" aria-labelledby="dropdownMenu">
-          <li><a href="#" onClick={e => {e.preventDefault(); setMarketFilter('G3_PLUS_C')}}>G3 + China</a></li>
-          <li><a href="#" onClick={e => {e.preventDefault(); setMarketFilter('HIGH_INCOME_EAST_ASIA')}}>High Income East Asia</a></li>
-          <li><a href="#" onClick={e => {e.preventDefault(); setMarketFilter('MIDDLE_INCOME_EAST_ASIA')}}>Middle Income East Asia</a></li>
-          <li><a href="#" onClick={e => {e.preventDefault(); setMarketFilter('ALL_EAST_ASIA')}}>All East Asia</a></li>
+          <li><a href="#" onClick={e => { e.preventDefault(); setMarketFilter('G3_PLUS_C'); }}>G3 + China</a></li>
+          <li><a href="#" onClick={e => { e.preventDefault(); setMarketFilter('HIGH_INCOME_EAST_ASIA'); }}>High Income East Asia</a></li>
+          <li><a href="#" onClick={e => { e.preventDefault(); setMarketFilter('MIDDLE_INCOME_EAST_ASIA'); }}>Middle Income East Asia</a></li>
+          <li><a href="#" onClick={e => { e.preventDefault(); setMarketFilter('ALL_EAST_ASIA'); }}>All East Asia</a></li>
         </ul>
       </div>
-    )
+    );
   }
 }
 
-export default MarketFilter
+MarketFilter.propTypes = propTypes;
+
+export default MarketFilter;
