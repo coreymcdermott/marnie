819d344d66a531bae2e10d0937c652283fa513c8
Corey McDermott <corey@mcdermott.id.au>
2016-11-26 11:48:18 +1100

    Fixed all markets filter.

--- src/components/MarketFilter.jsx
+++ src/components/MarketFilter.jsx
@@ -10,10 +10,11 @@ class MarketFilter extends Component {
     return (
       <div className="dropdown pull-right">
         <button className="btn btn-default dropdown-toggle" type="button" id="dropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-          All Markets&nbsp;
+          Market Filter&nbsp;
           <span className="caret"></span>
         </button>
         <ul className="dropdown-menu" aria-labelledby="dropdownMenu">
+          <li><a href="#" onClick={e => { e.preventDefault(); setMarketFilter('ALL'); }}>All</a></li>
           <li><a href="#" onClick={e => { e.preventDefault(); setMarketFilter('G3_PLUS_C'); }}>G3 + China</a></li>
           <li><a href="#" onClick={e => { e.preventDefault(); setMarketFilter('HIGH_INCOME_EAST_ASIA'); }}>High Income East Asia</a></li>
           <li><a href="#" onClick={e => { e.preventDefault(); setMarketFilter('MIDDLE_INCOME_EAST_ASIA'); }}>Middle Income East Asia</a></li>
