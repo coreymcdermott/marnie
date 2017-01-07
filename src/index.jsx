43f55b693db310ece228c0ba51c9b7ea71a67a34
Corey McDermott <corey@mcdermott.id.au>
2017-01-07 17:56:01 +1100

    Bootstrap and CSS now loaded via web pack.

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,9 @@ import promise                          from 'redux-promise';
 import createLogger                     from 'redux-logger';
 import rootReducer                      from './reducers';
 import CalendarContainer                from './containers/CalendarContainer.jsx';
+import bootstrap                        from 'bootstrap'
+import Bootstrap                        from 'bootstrap/dist/css/bootstrap.css';
+import Style                            from './style/style.css';
 
 let logger = createLogger();
 let store  = createStore(
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
+const path    = require('path');
+const webpack = require('webpack');
+
+
 module.exports = {
   entry: './src/index.jsx',
   output: {
@@ -12,14 +16,32 @@ module.exports = {
       query: {
         presets: ['es2015', 'react'],
       },
+    }, {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader',
+    }, {
+      test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?name=[name].[ext]limit=10000&mimetype=application/font-woff',
+    }, {
+      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?name=[name].[ext]limit=10000&mimetype=application/octet-stream',
+    }, {
+      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?name=[name].[ext]limit=10000&mimetype=application/octet-stream',
+    }, {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?name=[name].[ext]limit=10000&mimetype=image/svg+xml',
     }],
   },
   node: {
     fs: 'empty',
   },
-  externals: [
-    {
-      './cptable': 'var cptable',
-    },
-  ],
+  externals: [{
+    './cptable': 'var cptable',
+  }],
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    })],
 };
